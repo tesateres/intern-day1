7204ce886150409efd1bfc7c975ac92fc09cd205
tesateres <104637696+tesateres@users.noreply.github.com>
2023-05-02 15:49:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,133 @@
+//console.log("hello world");
+// var let const
+//var a=10;
+//console.log(a);
+//var a=20;
+//console.log(a);
+//{
+//let b=20;
+//console.log(b);
+//}
+//let b=30;
+//console.log(b);
+/*let a=10;
+let b=30;
+console.log(a+b);
+let c=20;
+let d=30;
+console.log(c-d);
+console.log(d/a);
+console.log(a*d);
+console.log(a%b);*/
+/*let a=10;
+let b="10";//=== checks datatype
+if(a===b)
+{
+    console.log(true)
+}else{
+    console.log(false)
+}
+let f="Tesa ";
+let g="Teres";
+let FN=f+g;
+console.log(FN);
+console.log(`My name is ${f}${g}`);
+let car={
+    model:"Celerio",
+    color:"white",
+    price:1000000
+}
+console.log(car);
+console.log(car.color);
+console.log(`${car.model} ${car.color}`);
+console.log(car.model+car.color);
+let person={
+    name:"Tesa",
+    college:"Christ College of Engineering",
+    Class:"S4 CSE",
+    address:{
+        housename:"abc",
+        street:"Pallath lane"
+    }
+}
+console.log(`${person.name} ${person.address.housename}`);
+let fruits=["apple","orange","mango",10]
+fruits[3]="grapes";
+console.log(fruits);
+fruits.push("pineapple")
+fruits.unshift("dragon fruit")
+fruits.pop()
+console.log(fruits);
+let todo=[
+    {id :1,
+    task:"buy book"
+},
+{
+    id:2,
+    task:"play"
+},
+{
+    id:3,
+    task:"read"
+},
+{
+    id:4,
+    task:"sleep"
+}
+]
+console.log(todo) 
+
+let students=[
+    {name:"arun",age:20},{name:"tesa",age:21}
+]
+console.log(students[1].name)
+function addTwoNumbers(a,b) {
+    console.log(a+b);
+}
+function subtract(a,b){
+    console.log(a-b);
+}
+function product(a,b) {
+    console.log(a*b);
+}
+
+
+subtract(50,40);
+product(9,12);
+add = (a,b) =>{
+console.log(a+b)
+};
+add(20,30);
+addTwoNumbers(10,20);
+sub =(a,b) =>{
+    console.log(a-b)
+};
+sub(20,7);
+function add(a,b){
+    let sum =a+b;
+    if(sum>10)
+    {
+      console.log("sum is greater than 10");
+
+    }
+    else{
+        console.log("sum is less than 10");
+    }
+};
+add(10,20);*/
+let numbers=[1,2,3,4,5,6,7,8,9,10]
+//console.log(numbers);
+//for(let i=1;i<numbers.length;i++)
+//{console.log(i);
+//}
+arraywhile =(numbers)=>{
+let i=0;
+while(i<numbers.length)
+{
+    console.log(numbers[i]);
+    i=i+1
+    }
+
+};
+arraywhile(numbers);
+
